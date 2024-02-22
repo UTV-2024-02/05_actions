8862c44b7fb199241aaf4148f92dc75c7656e394
Peter Dillinger <peter.dillinger@srh.de>
2024-02-22 08:11:57 +0000

    some more tests added

---
+++ calculation.test.js
@@ -0,0 +1,45 @@
+const calculate = require('./calculation');
+
+describe('Testing unit calculation',
+    () => {
+        it(
+            'Check for standard division',
+            () => {
+                expect( calculate(8, 2) ).toBe( 4 );
+            });
+
+        it(
+            'Division by 1',
+            () => {
+                expect( calculate(7, 1) ).toBe( 7 );
+            }
+        );
+
+        it(
+            'Division by zero',
+            () => {
+                expect( calculate(3, 0) ).toBe( Infinity );
+            }
+        );
+
+        it(
+            'Zero division by anything',
+            () => {
+                expect( calculate(0, 13) ).toBe( 0 );
+            }
+        );
+
+        it(
+            'Random tests',
+            () => {
+                for (let i=0; i<100; i++) {
+                    let a = Math.random() * 100 + 1;
+                    let b = Math.random() * 100 + 1;
+                    let result = a / b;
+                    expect( calculate(a, b) ).toBe( result );
+                }
+            }
+        )
+
+        }
+);
